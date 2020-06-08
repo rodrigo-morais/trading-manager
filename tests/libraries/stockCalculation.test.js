98b12d3c3f50bea657a391158845a078cf20fa81
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-06-08 19:44:36 +0200

    Adding tests to calculate the difference from start to stop in an operation.

--- tests/libraries/stockCalculation.test.js
+++ tests/libraries/stockCalculation.test.js
@@ -1,4 +1,4 @@
-import { getDirection } from '../../src/libraries/stockCalculation'
+import { getDirection, getDifference } from '../../src/libraries/stockCalculation'
 
 describe('stockCalculation', () => {
   describe('getDirection', () => {
@@ -21,6 +21,22 @@ describe('stockCalculation', () => {
       const stop = 10
 
       expect(getDirection(start, stop)).toEqual('Venda')
+    })
+  })
+
+  describe('getDifference', () => {
+    it('returns the absolute difference when stop is bigger than start', () => {
+      const start = 10
+      const stop = 14
+
+      expect(getDifference(start, stop)).toEqual(4)
+    })
+
+    it('returns the abolute difference when stop is smaller than start', () => {
+      const start = 14
+      const stop = 10
+
+      expect(getDifference(start, stop)).toEqual(4)
     })
   })
 })
