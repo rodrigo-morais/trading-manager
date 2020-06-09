99a67940b1daa92ce8e65bfb2ca06421e0bd8647
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-06-09 20:50:48 +0200

    Adding "stocks" to trades reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import trades from '../containers/Trades/reducer'
 import systems from './systems'
+import stocks from './stocks'
 
 export default {
+  stocks,
   systems,
   trades,
 }
---
+++ src/reducers/stocks.js
@@ -0,0 +1,14 @@
+const initialState = [
+  {
+    acronyms: 'WDO',
+    valuePerTick: 10,
+  },
+  {
+    acronyms: 'WIN',
+    valuePerTick: 0.2,
+  },
+]
+
+const stocksReducer = (state = initialState) => state
+
+export default stocksReducer
