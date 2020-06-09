6f8c0286306866aa9c3f743643467a285bad533f
Rodrigo Moreira de Morais <morais.rm@gmail.com>
2020-06-09 20:44:05 +0200

    Changing "getRisk" function to receive the value per tick.

--- src/libraries/stockCalculation.js
+++ src/libraries/stockCalculation.js
@@ -2,10 +2,10 @@ const getDirection = (start, stop) => start > stop ? 'Venda' : 'Compra'
 
 const getDifference = (start, stop) => Math.abs(start - stop)
 
-const getRisk = (stock, difference) => {
+const getRisk = (stock, difference, valuePerTick) => {
   switch (stock.substr(0, 3)) {
-    case 'WIN': return difference * 0.2
-    case 'WDO': return difference * 10
+    case 'WIN': return difference * valuePerTick
+    case 'WDO': return difference * valuePerTick
     default: return difference
   }
 }
--- tests/libraries/stockCalculation.test.js
+++ tests/libraries/stockCalculation.test.js
@@ -44,22 +44,25 @@ describe('stockCalculation', () => {
     it('returns the risk when stock is Brazilian Index', () => {
       const stock = 'WINM20'
       const difference = 100
+      const valuePerTick = 0.2
 
-      expect(getRisk(stock, difference)).toEqual(20)
+      expect(getRisk(stock, difference, valuePerTick)).toEqual(20)
     })
 
     it('returns the risk when stock is Dollar', () => {
       const stock = 'WDOM20'
       const difference = 10
+      const valuePerTick = 10
 
-      expect(getRisk(stock, difference)).toEqual(100)
+      expect(getRisk(stock, difference, valuePerTick)).toEqual(100)
     })
 
     it('returns the risk when stock is not Brazilian Index or Dollar', () => {
       const stock = 'GGBR4'
       const difference = 10
+      const valuePerTick = 1
 
-      expect(getRisk(stock, difference)).toEqual(10)
+      expect(getRisk(stock, difference, valuePerTick)).toEqual(10)
     })
   })
 })
